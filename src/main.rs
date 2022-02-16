cd33b69d417918f822dbf53bfab8c5a4b3b9d050
Owen O'Malley <omalley@apache.org>
2022-02-15 21:07:33 -0800

    puzzle 4.1

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,59 @@
+use std::collections::HashMap;
 use std::io;
 use std::io::BufRead;
 
 #[derive(Debug)]
-struct Field {
-  trees: Vec<Vec<bool>>,
-  width: usize,
-  height: usize,
+struct Passport {
+  fields: HashMap<String, String>,
 }
 
-impl Field {
-  fn parse(lines: &mut dyn Iterator<Item=String>) -> Self {
-    let trees: Vec<Vec<bool>> = lines
-      .map(|l| l.chars().map(|c| c == '#').collect())
-      .collect();
-    let width = trees[0].len();
-    let height = trees.len();
-    Field{trees, width, height}
+impl Passport {
+  fn parse(lines: &mut dyn Iterator<Item=String>) -> Option<Self> {
+    let mut fields: HashMap<String, String> = HashMap::new();
+    loop {
+      match lines.next() {
+        Some(line) => {
+          if line.is_empty() {
+            break
+          }
+          for word in line.split_ascii_whitespace() {
+            let mut parts = word.split(":");
+            fields.insert(String::from(parts.next().unwrap()),
+                          String::from(parts.next().unwrap()));
+          }
+        }
+        None => break,
+      }
+    }
+    if fields.is_empty() {
+      None
+    } else {
+      Some(Passport{fields})
+    }
   }
 
-  fn trees_hit(&self, path: (usize, usize)) -> usize {
-    let mut x = 0;
-    let mut y = 0;
-    let mut trees = 0;
-    while y < self.height {
-      if self.trees[y][x] {
-        trees += 1;
+  const FIELDS: &'static[&'static str] = &["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+
+  fn is_valid(&self) -> bool {
+    let mut count = 0;
+    for &k in Self::FIELDS {
+      if self.fields.contains_key(k) {
+        count += 1;
       }
-      x = (x + path.0) % self.width;
-      y += path.1;
     }
-    trees
+    count == Self::FIELDS.len()
   }
 }
 
 fn main() {
   let stdin = io::stdin();
-  let field = Field::parse(&mut stdin.lock().lines()
-    .map(|x| String::from(x.unwrap().trim())));
-  let mut product = 1;
-  for path in vec![(1,1), (3,1), (5,1), (7,1), (1, 2)] {
-    let hits = field.trees_hit(path);
-    println!("{:?} = {}", path, hits);
-    product *= hits;
+  let mut input = stdin.lock().lines()
+    .map(|x| String::from(x.unwrap().trim()));
+  let mut count = 0;
+  while let Some(passport) = Passport::parse(&mut input) {
+    if passport.is_valid() {
+      count += 1;
+    }
   }
-  println!("product = {}", product);
+  println!("{}", count);
 }
