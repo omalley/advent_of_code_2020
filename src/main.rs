f830ce040be03a85ca497c1dda3e228b11eb8bed
Owen O'Malley <omalley@apache.org>
2022-02-15 22:07:20 -0800

    puzzle 5.1

--- src/main.rs
+++ src/main.rs
@@ -1,118 +1,34 @@
-use std::collections::HashMap;
 use std::io;
 use std::io::BufRead;
 
-use lazy_static::lazy_static;
-use regex::Regex;
-
 #[derive(Debug)]
-struct Passport {
-  fields: HashMap<String, String>,
+struct BoardingCard {
+  row: usize,
+  seat: usize,
 }
 
-impl Passport {
-  fn parse(lines: &mut dyn Iterator<Item=String>) -> Option<Self> {
-    let mut fields: HashMap<String, String> = HashMap::new();
-    loop {
-      match lines.next() {
-        Some(line) => {
-          if line.is_empty() {
-            break
-          }
-          for word in line.split_ascii_whitespace() {
-            let mut parts = word.split(":");
-            fields.insert(String::from(parts.next().unwrap()),
-                          String::from(parts.next().unwrap()));
-          }
-        }
-        None => break,
-      }
-    }
-    if fields.is_empty() {
-      None
-    } else {
-      Some(Passport{fields})
-    }
+impl BoardingCard {
+  fn parse(line: &str) -> Self {
+    let chars: Vec<char> = line.chars().collect();
+    let row = chars[..7].iter()
+      .map(|&c| if c == 'F' {0} else {1})
+      .fold(0, |acc, v| 2 * acc + v);
+    let seat = chars[7..].iter()
+      .map(|&c| if c == 'L' {0} else {1})
+      .fold(0, |acc, v| 2 * acc + v);
+    BoardingCard{row, seat}
   }
 
-  fn is_valid_year(year: Option<&String>, lower: i64, upper: i64) -> bool {
-    lazy_static! {
-      static ref YEAR_PATTERN: Regex = Regex::new(r"^\d{4}$").unwrap();
-    }
-    if let Some(yr) = year {
-      if YEAR_PATTERN.is_match(yr) {
-        let num = yr.parse::<i64>().unwrap();
-        return lower <= num && num <= upper
-      }
-    }
-    false
-  }
-
-  fn is_valid_height(str: Option<&String>) -> bool {
-    lazy_static! {
-      static ref HEIGHT_PATTERN: Regex = Regex::new(r"^(?P<num>\d+)(?P<unit>cm|in)$").unwrap();
-    }
-    if let Some(s) = str {
-      if let Some(captures) = HEIGHT_PATTERN.captures(s) {
-        let num = captures.name("num").unwrap().as_str().parse::<i64>().unwrap();
-        let unit = captures.name("unit").unwrap().as_str();
-        match unit {
-          "in" => return 59 <= num && num <= 76,
-          "cm" => return 150 <= num && num <= 193,
-          _ => {}
-        }
-      }
-    }
-    false
-  }
-
-  fn matches_regex(str: Option<&String>, regex: &Regex) -> bool {
-    if let Some(s) = str {
-      return regex.is_match(s)
-    }
-    false
-  }
-
-  fn is_valid(&self) -> bool {
-    lazy_static! {
-      static ref HAIR_PATTERN: Regex = Regex::new(r"^#[0-9a-f]{6}$").unwrap();
-      static ref EYE_PATTERN: Regex = Regex::new(r"^amb|blu|brn|gry|grn|hzl|oth$").unwrap();
-      static ref PASSPORT_PATTERN: Regex = Regex::new(r"^\d{9}$").unwrap();
-    }
-    if !Self::is_valid_year(self.fields.get("byr"), 1920, 2002) {
-      return false
-    }
-    if !Self::is_valid_year(self.fields.get("iyr"), 2010, 2020) {
-      return false
-    }
-    if !Self::is_valid_year(self.fields.get("eyr"), 2020, 2030) {
-      return false
-    }
-    if !Self::is_valid_height(self.fields.get("hgt")) {
-      return false
-    }
-    if !Self::matches_regex(self.fields.get("hcl"), &HAIR_PATTERN) {
-      return false
-    }
-    if !Self::matches_regex(self.fields.get("ecl"), &EYE_PATTERN) {
-      return false
-    }
-    if !Self::matches_regex(self.fields.get("pid"), &PASSPORT_PATTERN) {
-      return false
-    }
-    true
+  fn seat_id(&self) -> usize {
+    self.row * 8 + self.seat
   }
 }
 
 fn main() {
   let stdin = io::stdin();
-  let mut input = stdin.lock().lines()
-    .map(|x| String::from(x.unwrap().trim()));
-  let mut count = 0;
-  while let Some(passport) = Passport::parse(&mut input) {
-    if passport.is_valid() {
-      count += 1;
-    }
-  }
-  println!("{}", count);
+  let cards: Vec<BoardingCard> = stdin.lock().lines()
+    .map(|x| BoardingCard::parse(x.unwrap().trim()))
+    .collect();
+  println!("max = {:?}", cards.iter().map(|s| s.seat_id())
+    .reduce(|l,r| usize::max(l,r )));
 }
