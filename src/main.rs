9c4b91987a3ac34439249cd0836fd22ce6e75992
Owen O'Malley <omalley@apache.org>
2022-02-15 20:16:11 -0800

    puzzle 3.1

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,41 @@
 use std::io;
 use std::io::BufRead;
 
-use lazy_static::lazy_static;
-use regex::Regex;
-
 #[derive(Debug)]
-struct Rule {
-  lower: usize,
-  upper: usize,
-  goal: char,
-  password: String,
+struct Field {
+  trees: Vec<Vec<bool>>,
+  width: usize,
+  height: usize,
 }
 
-impl Rule {
-  fn parse(line: &str) -> Option<Self> {
-    lazy_static! {
-        static ref PASSWORD_PATTERN: Regex = Regex::new(
-            r"^(?P<lower>\d+)-(?P<upper>\d+) (?P<char>.):\s*(?P<pass>\w+)$").unwrap();
-    }
-    let cap = PASSWORD_PATTERN.captures(line)?;
-    let lower = cap.name("lower")?.as_str().parse::<usize>().unwrap();
-    let upper = cap.name("upper")?.as_str().parse::<usize>().unwrap();
-    let goal = cap.name("char")?.as_str().chars().next()?;
-    let password = String::from(cap.name("pass")?.as_str());
-    Some(Rule{lower, upper, goal, password})
+impl Field {
+  fn parse(lines: &mut dyn Iterator<Item=String>) -> Self {
+    let trees: Vec<Vec<bool>> = lines
+      .map(|l| l.chars().map(|c| c == '#').collect())
+      .collect();
+    let width = trees[0].len();
+    let height = trees.len();
+    Field{trees, width, height}
   }
 
-  fn compliant(&self) -> bool {
-    let chars: Vec<char> = self.password.chars().collect();
-    (chars[self.lower - 1] == self.goal) != (chars[self.upper - 1] == self.goal)
+  fn trees_hit(&self, path: (usize, usize)) -> usize {
+    let mut x = 0;
+    let mut y = 0;
+    let mut trees = 0;
+    while y < self.height {
+      if self.trees[y][x] {
+        trees += 1;
+      }
+      x = (x + path.0) % self.width;
+      y += path.1;
+    }
+    trees
   }
 }
 
 fn main() {
   let stdin = io::stdin();
-  let rules: Vec<Rule> = stdin.lock().lines()
-    .map(|x| Rule::parse(x.unwrap().trim()).unwrap())
-    .filter(|r| r.compliant())
-    .collect();
-  println!("{}", rules.len());
+  let field = Field::parse(&mut stdin.lock().lines()
+    .map(|x| String::from(x.unwrap().trim())));
+  println!("{}", field.trees_hit((3, 1)));
 }
