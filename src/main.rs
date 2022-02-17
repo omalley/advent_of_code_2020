ecabf9119a376cdc1b8d96bf54894d96643e3ca7
Owen O'Malley <omalley@apache.org>
2022-02-16 23:59:30 -0800

    puzzle 8.1

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,45 @@
-use std::collections::HashMap;
 use std::io;
 use std::io::BufRead;
 
-use lazy_static::lazy_static;
-use regex::Regex;
-
 #[derive(Debug)]
-struct Bag {
-  contains: Vec<BagCount>,
+enum Instruction {
+  Acc(i64),
+  Jmp(i64),
+  Nop,
 }
 
-#[derive(Debug)]
-struct BagCount {
-  count: u64,
-  bag: String,
-}
-
-#[derive(Debug)]
-struct Rules {
-  rules: HashMap<String,Bag>,
-  contained_in: HashMap<String, Vec<String>>,
-}
-
-impl Rules {
-  fn add_parent(&mut self, parent: &str, child: &str) {
-    if !self.contained_in.contains_key(child) {
-      self.contained_in.insert(child.to_string(), Vec::new());
+impl Instruction {
+  fn parse(line: &str) -> Option<Self> {
+    let words : Vec<&str> = line.split_ascii_whitespace().collect();
+    let arg = words[1].parse::<i64>().unwrap();
+    match words[0] {
+      "nop" => Some(Self::Nop),
+      "acc" => Some(Self::Acc(arg)),
+      "jmp" => Some(Self::Jmp(arg)),
+      _ => None,
     }
-    self.contained_in.get_mut(child).unwrap().push(parent.to_string());
-  }
-
-  fn parse_bag(&mut self, line: &str) {
-    lazy_static! {
-      static ref LINE_PATTERN: Regex = Regex::new(
-        r"^(?P<name>.+) bags contain (?P<contains>.+)[.]$").unwrap();
-      static ref CHILD_PATTERN: Regex = Regex::new(r"^(?P<count>\d+) (?P<bag>.+) bags?$").unwrap();
-    }
-    let capture = LINE_PATTERN.captures(line).unwrap();
-    let name = capture.name("name").unwrap().as_str().to_string();
-    let tail = capture.name("contains").unwrap().as_str().to_string();
-    let mut contains = Vec::new();
-    if tail != "no other bags" {
-      for bag in tail.split(", ") {
-        let bag_cap = CHILD_PATTERN.captures(bag).unwrap();
-        let bag = bag_cap.name("bag").unwrap().as_str().to_string();
-        let count = bag_cap.name("count").unwrap().as_str().parse::<u64>().unwrap();
-        contains.push(BagCount{count, bag: bag.clone()});
-        self.add_parent(&name, &bag);
-      }
-    }
-    self.rules.insert(name,Bag{contains});
-  }
-
-  fn parse(input: &mut dyn Iterator<Item=String>) -> Self {
-    let mut result = Rules{rules: HashMap::new(), contained_in: HashMap::new()};
-    for line in input {
-      result.parse_bag(&line);
-    }
-    result
-  }
-
-  fn count(&self, count: u64, color:& str) -> u64 {
-    let result = count + self.rules.get(color).unwrap().contains.iter()
-      .map(|b| self.count(count * b.count, &b.bag))
-      .fold(0, |a,b| a + b);
-    println!("{} {} = {}", color, count, result);
-    result
   }
 }
 
+fn run_program(pgm: &Vec<Instruction>) -> i64 {
+  let mut done = vec![false; pgm.len()];
+  let mut acc: i64 = 0;
+  let mut pc: usize = 0;
+  while !done[pc] {
+    println!("pc = {}, acc = {}", pc, acc);
+    done[pc] = true;
+    match pgm[pc] {
+      Instruction::Nop => {pc += 1},
+      Instruction::Acc(val) => {acc += val; pc += 1},
+      Instruction::Jmp(val) => pc = (pc as i64 + val) as usize,
+    }
+  }
+  acc
+}
 fn main() {
   let stdin = io::stdin();
-  let rules = Rules::parse(&mut stdin.lock().lines()
-    .map(|s| s.unwrap()));
-  println!("count = {}", rules.count(1, "shiny gold") - 1);
+  let program: Vec<Instruction> = stdin.lock().lines()
+    .map(|s| Instruction::parse(&s.unwrap()).unwrap())
+    .collect();
+  println!("count = {}", run_program(&program));
 }
