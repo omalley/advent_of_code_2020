9c9963fc70ee5e65c1b4a5e58294170fe044d95c
Owen O'Malley <omalley@apache.org>
2022-02-17 12:35:02 -0800

    puzzle 9.2

--- src/main.rs
+++ src/main.rs
@@ -19,9 +19,20 @@ fn main() {
   let input: Vec<i64> = stdin.lock().lines()
     .map(|s| s.unwrap().parse::<i64>().unwrap())
     .collect();
+  let mut unmatched = 0;
   for i in WINDOW_SIZE..input.len() {
     if !checksum(input[i], &input[i-WINDOW_SIZE..i]) {
-      println!("unmatched {} = {}", i, input[i]);
+      unmatched = input[i];
+      println!("unmatched {} = {}", i, unmatched);
+    }
+  }
+  for i0 in 0..input.len()-1 {
+    for i1 in i0+1..input.len() {
+      if input[i0..=i1].iter().fold(0, |a, b| a + b) == unmatched {
+        let min = input[i0..=i1].iter().min().unwrap();
+        let max = input[i0..=i1].iter().max().unwrap();
+        println!("{} .. {} = {} min + max = {}", input[i0], input[i1], unmatched, min + max);
+      }
     }
   }
 }
