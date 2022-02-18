95f6a2e3eb4dd84eed63926b5d53bb463cab50da
Owen O'Malley <omalley@apache.org>
2022-02-17 22:32:47 -0800

    puzzle 11.1

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,94 @@
-use std::collections::HashMap;
 use std::io;
 use std::io::BufRead;
 
-const MAX_SKIP: i64 = 3;
+#[derive(Debug, Eq, PartialEq)]
+enum Spot {
+  Floor,
+  Chair,
+  Person,
+}
 
-fn count_arrangements(cur: i64, remaining: &[i64],
-                      cache: &mut HashMap<(i64,usize), usize>) -> usize {
-  let key = (cur, remaining.len());
-  if cache.contains_key(&key) {
-    *cache.get(&key).unwrap()
-  } else if remaining.is_empty() {
-    1
-  } else {
-    let mut total = 0;
-    let mut next = 0;
-    while next < remaining.len() && remaining[next] - cur <= MAX_SKIP {
-      total += count_arrangements(remaining[next], &remaining[next+1..], cache);
-      next += 1;
+#[derive(Debug)]
+struct Floor {
+  seats: Vec<Vec<Spot>>,
+  width: usize,
+  occupied: Vec<Vec<bool>>,
+}
+
+impl Floor {
+  fn parse(input: &mut dyn Iterator<Item=String>) -> Self {
+    let mut seats: Vec<Vec<Spot>> = Vec::new();
+    for line in input {
+      let row = line.chars().map(|c|
+        match c {
+          '.' => Spot::Floor,
+          'L' => Spot::Chair,
+          _ => Spot::Person,
+        }).collect();
+      seats.push(row);
     }
-    cache.insert(key, total);
-    total
+    let width= seats[0].len();
+    let occupied = vec![vec![false; width]; seats.len()];
+    Floor{seats, width, occupied}
+  }
+
+  fn neighbors(&self, x: usize, y: usize) -> usize {
+    let mut result = 0;
+    for neighbor_x in (x as i64) - 1 ..= (x as i64) + 1 {
+      for neighbor_y in (y as i64) - 1 ..= (y as i64) + 1 {
+        if neighbor_x >= 0 && neighbor_x < self.width as i64 && neighbor_y >= 0 &&
+            neighbor_y < self.seats.len() as i64 &&
+            (neighbor_x != x as i64 || neighbor_y != y as i64) {
+          if self.occupied[neighbor_y as usize][neighbor_x as usize] {
+            result += 1;
+          }
+        }
+      }
+    }
+    result
+  }
+
+  fn update(&mut self) -> bool {
+    let mut next = self.occupied.clone();
+    let mut result = false;
+    for y in 0..next.len() {
+      for x in 0..self.width {
+        if self.occupied[y][x] {
+          if self.neighbors(x,y) >= 4 {
+            result = true;
+            next[y][x] = false;
+          }
+        } else if self.seats[y][x] == Spot::Chair && self.neighbors(x,y) == 0 {
+          result = true;
+          next[y][x] = true;
+        }
+      }
+    }
+    if result {
+      self.occupied = next;
+    }
+    result
+  }
+
+  fn count(&self) -> usize {
+    let mut result = 0;
+    for y in 0..self.occupied.len() {
+      for x in 0..self.width {
+        if self.occupied[y][x] {
+          result += 1;
+        }
+      }
+    }
+    result
   }
 }
 
 fn main() {
   let stdin = io::stdin();
-  let mut input: Vec<i64> = stdin.lock().lines()
-    .map(|s| s.unwrap().parse::<i64>().unwrap())
-    .collect();
-  input.sort();
-  let mut cache: HashMap<(i64,usize), usize> = HashMap::new();
-  println!("{}", count_arrangements(0, &input, &mut cache));
+  let mut map: Floor = Floor::parse(&mut stdin.lock().lines()
+    .map(|s| s.unwrap()));
+  while map.update() {
+    // pass
+  }
+  println!("count = {}", map.count());
 }
