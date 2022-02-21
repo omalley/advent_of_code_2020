285da4bdc325d83dcf1c68b3ccd29736f3195914
Owen O'Malley <omalley@apache.org>
2022-02-20 20:52:02 -0800

    puzzle 18.1

--- src/main.rs
+++ src/main.rs
@@ -47,6 +47,58 @@ impl Lexer {
   }
 }
 
+#[derive(Clone, Debug)]
+enum Expression {
+  Number(i64),
+  Add(Box<Expression>, Box<Expression>),
+  Multiply(Box<Expression>, Box<Expression>),
+}
+
+impl Expression {
+  fn parse_term(input: &mut dyn Iterator<Item=&Token>) -> Option<Expression> {
+    if let Some(head) = input.next() {
+      match head {
+        Token::Number(i) => Some(Expression::Number(*i)),
+        Token::Left => Some(Self::parse(input)),
+        _ => panic!("Illegal token - {:?}", head),
+      }
+    } else {
+      None
+    }
+  }
+
+  fn parse(input: &mut dyn Iterator<Item=&Token>) -> Expression {
+    let mut current = Self::parse_term(input).unwrap();
+    while let Some(head) = input.next() {
+      match &head {
+        Token::Add => {
+          let right = Self::parse_term(input).unwrap();
+          current = Expression::Add(Box::new(current),
+                                    Box::new(right))
+        },
+        Token::Multiply => {
+          let right = Self::parse_term(input).unwrap();
+          current = Expression::Multiply(Box::new(current),
+                                         Box::new(right))
+        },
+        Token::Right => break,
+        _ => panic!("Bad token {:?}", head),
+      }
+    }
+    current
+  }
+
+  fn evaluate(&self) -> i64 {
+    match self {
+      Expression::Number(i) => *i,
+      Expression::Add(left, right) =>
+        left.evaluate() + right.evaluate(),
+      Expression::Multiply(left, right) =>
+        left.evaluate() * right.evaluate(),
+    }
+  }
+}
+
 fn main() {
   let stdin = io::stdin();
   let lines: Vec<String> = stdin.lock().lines()
@@ -54,7 +106,12 @@ fn main() {
     .filter(|x| !x.is_empty())
     .collect();
   let lexer = Lexer::new();
+  let mut sum = 0;
   for line in lines {
-    println!("{:?}", lexer.parse(&line));
+    let expr = Expression::parse(&mut lexer.parse(&line).iter());
+    let val = expr.evaluate();
+    println!("{}", val);
+    sum += val;
   }
+  println!("sum = {}", sum);
 }
