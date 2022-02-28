36c56e1d8132d847ba409a810247691bf510b3ab
Owen O'Malley <omalley@apache.org>
2022-02-27 19:58:45 -0800

    puzzle 20.1

--- src/main.rs
+++ src/main.rs
@@ -1,177 +1,117 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::io;
 use std::io::BufRead;
 
-#[derive(Clone, Debug, Eq, Hash, PartialEq)]
-enum Item {
-  Ref(usize),
-  Letter(char),
+use lazy_static::lazy_static;
+use regex::Regex;
+
+#[derive(Debug)]
+struct Tile {
+  id: i64,
+  pixels: Vec<Vec<bool>>,
+  width: usize,
 }
 
-impl Item {
-  fn parse(s: &str) -> Self {
-    let s = s.trim();
-    if let Ok(i) = s.parse() {
-      Item::Ref(i)
-    } else {
-      let parts: Vec<char> = s.chars().collect();
-      Item::Letter(parts[1])
+impl Tile {
+  fn parse(input: &mut dyn Iterator<Item=String>) -> Option<Self> {
+    lazy_static! {
+      static ref HEADER: Regex = Regex::new(r"^Tile\s+(?P<id>\d+):$").unwrap();
     }
-  }
-}
-
-#[derive(Debug)]
-struct Sequence {
-  items: Vec<Item>,
-}
-
-impl Sequence {
-  fn parse(s: &str) -> Self {
-    let items = s.trim().split_ascii_whitespace()
-      .map(|x| Item::parse(x)).collect();
-    Sequence{items}
-  }
-}
-
-#[derive(Debug)]
-struct Rule {
-  name: usize,
-  alternatives: Vec<Sequence>,
-}
-
-impl Rule {
-  fn parse(s: &str) -> Self {
-    let mut parts = s.split(":");
-    let name: usize = *&parts.next().unwrap().trim().parse().unwrap();
-    let alternatives = parts.next().unwrap().trim().split("|")
-      .map(|x| Sequence::parse(x)).collect();
-    Rule{name, alternatives}
-  }
-}
-
-#[derive(Debug)]
-struct Input {
-  rules: HashMap<usize, Rule>,
-  strings: Vec<String>,
-  singletons: HashMap<Item, Vec<usize>>,
-}
-
-impl Input {
-  fn parse(input: &mut dyn Iterator<Item=String>) -> Self {
-    let mut rules = HashMap::new();
-    let mut singletons: HashMap<Item, Vec<usize>> = HashMap::new();
+    let first_line = input.next()?;
+    let id_match = HEADER.captures(&first_line)?;
+    let id = id_match.name("id").unwrap().as_str().parse::<i64>().unwrap();
+    let mut pixels = Vec::new();
     while let Some(line) = input.next() {
       if line.is_empty() {
         break;
+      }
+      pixels.push(line.chars().map(|c| c == '#').collect());
+    }
+    let width = pixels.len();
+    Some(Tile{id, pixels, width})
+  }
+
+  fn normalize(&self, val: u64) -> u64 {
+    let mut result = 0;
+    for i in 0..self.width {
+      result = (result << 1) | ((val >> i) & 1);
+    }
+    u64::min(val, result)
+  }
+
+  fn convert_row(&self, row: usize) -> u64 {
+    self.pixels[row].iter().map(|x| if *x {1} else {0})
+      .fold(0, |acc, x| acc << 1 | x)
+  }
+
+  fn convert_column(&self, column: usize) -> u64 {
+    (0..self.pixels.len()).map(|r| if self.pixels[r][column] {1} else {0})
+      .fold(0, |acc, x| acc << 1 | x)
+  }
+
+  // Return the four edges as bit vectors
+  fn edges(&self) -> [u64; 4] {
+    [self.convert_row(0), self.convert_column(0),
+      self.convert_row(self.width - 1), self.convert_column(self.width -1)]
+  }
+}
+
+// Group the tiles by the normalized value of each of their edges
+fn find_edges(input: &Vec<Tile>) -> HashMap<u64, Vec<i64>> {
+  // find the tiles that share edges
+  let mut edges: HashMap<u64, Vec<i64>> = HashMap::new();
+  for t in input {
+    for e in t.edges() {
+      let norm = t.normalize(e);
+      if let Some(old) = edges.get_mut(&norm) {
+        old.push(t.id);
       } else {
-        let rule = Rule::parse(&line);
-        for a in &rule.alternatives {
-          if a.items.len() == 1 {
-            let key = &a.items[0];
-            if let Some(old) = singletons.get_mut(key) {
-              old.push(rule.name);
-            } else {
-              singletons.insert(key.clone(), vec![rule.name]);
-            }
-          }
-        }
-        let name = rule.name;
-        rules.insert(name, rule);
+        edges.insert(norm, vec![t.id]);
       }
     }
-    Input{rules, singletons, strings: input.collect()}
   }
+  edges
+}
 
-  fn match_alternative(&self, alt: &[Item],
-                       start: usize, end: usize,
-                       chars: &Vec<char>,
-                       subs: &Vec<Vec<HashSet<usize>>>) -> bool {
-    //println!("match alternative {:?} to {:?} ({}..={})", alt, chars[start..=end].iter().collect::<String>(), start, end);
-    let length = end - start + 1;
-    if alt.len() > 0 && alt.len() <= length {
-      let head = &alt[0];
-      let tail = &alt[1..];
-      match head {
-        Item::Letter(c) => if *c == chars[start] {
-          if tail.is_empty() && length == 1 {
-            //println!("matched literal");
-            return true
+// Given the grouping by edges, find the neighbors of each tile
+fn find_neighbors(edges: &HashMap<u64, Vec<i64>>) -> HashMap<i64, Vec<i64>> {
+  let mut result: HashMap<i64, Vec<i64>> = HashMap::new();
+  for (_, tiles) in edges {
+    for t in tiles {
+      for other in tiles {
+        if t != other {
+          if let Some(old) = result.get_mut(&t) {
+            old.push(*other);
           } else {
-            let result = self.match_alternative(tail, start + 1, end, chars, subs);
-            //println!("literal & pattern = {}", result);
-            return result
-          }
-        },
-        Item::Ref(r) => {
-          if tail.is_empty() {
-            let result = subs[length - 1][start].contains(r);
-            //println!("tail pattern = {}", result);
-            return result
-          } else {
-            for child_end in start..= end - tail.len() {
-              if subs[child_end - start][start].contains(r) {
-                if self.match_alternative(tail, child_end+1, end, chars, subs) {
-                  //println!("Found match at {}", child_end);
-                  return true
-                }
-              }
-            }
-          }
-        },
-      }
-    }
-    //println!("return false");
-    false
-  }
-
-  fn check(&self, s: &str) -> HashSet<usize> {
-    let chars: Vec<char> = s.chars().collect();
-    let mut result = vec![vec![HashSet::new(); chars.len()]; chars.len()];
-    for length in 1..=chars.len() {
-      for start in 0..=chars.len() - length {
-        let mut to_do: Vec<usize> = Vec::new();
-
-        // check each rule to see what we can match
-        for rule in self.rules.values() {
-          for alt in &rule.alternatives {
-            if self.match_alternative(&alt.items[..], start, start + length - 1,
-                                      &chars,&result) {
-              to_do.push(rule.name);
-              // once a rule is matched, we can check the next rule
-              break;
-            }
-          }
-        }
-
-        // for rules that match singletons, they get checked at the end here.
-        while let Some(r) = to_do.pop() {
-          if result[length - 1][start].insert(r) {
-            if let Some(more) = self.singletons.get(&Item::Ref(r)) {
-              to_do.append(&mut more.clone());
-            }
+            result.insert(*t, vec![*other]);
           }
         }
       }
     }
-    result[chars.len()-1][0].to_owned()
   }
+  result
+}
+
+fn parse(input: &mut dyn Iterator<Item=String>) -> Vec<Tile> {
+  let mut result = Vec::new();
+  while let Some(t) = Tile::parse(input) {
+    result.push(t);
+  }
+  result
 }
 
 fn main() {
   let stdin = io::stdin();
-  let input = Input::parse( & mut stdin.lock().lines()
+  let input = parse( & mut stdin.lock().lines()
     .map(|x| x.unwrap().trim().to_string()));
-  for r in input.rules.values() {
-    println!("rule: {:?}", r);
+  let index = find_neighbors(&find_edges(&input));
+  for (k,v) in &index {
+    println!("{} -> {:?}", k, v);
   }
-
-  let mut count = 0;
-  for str in &input.strings {
-    let result = input.check(str).contains(&0);
-    println!("{} = {}", str, result);
-    if result {
-      count += 1;
-    }
-  }
-  println!("count = {}", count);
+  // find the tiles with only two neighbors since they are the corners
+  // multiply their ids together
+  let result: i64 = (&index).iter()
+    .filter_map(|(k,v)| if v.len() == 2 {Some(k)} else {None})
+    .product();
+  println!("result = {}", result);
 }
