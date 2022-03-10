86995c06820ac2fa6f23b97f60fa29c94a9d8e27
Owen O'Malley <omalley@apache.org>
2022-03-09 20:33:29 -0800

    puzzle 22.2

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use std::collections::HashSet;
 use std::io;
 use std::io::BufRead;
 
 #[derive(Debug)]
 struct WarGame {
   players: Vec<Vec<i32>>,
+  previous: HashSet<Vec<Vec<i32>>>,
 }
 
 impl WarGame {
@@ -23,22 +25,63 @@ impl WarGame {
       }
       players.push(hand);
     }
-    WarGame{players}
+    WarGame{players, previous: HashSet::new()}
   }
 
-  fn next(&mut self) {
+  fn can_recurse(&self, hand: &Vec<i32>) -> bool {
+    for p in 0..hand.len() {
+      if (self.players[p].len() as i32) < hand[p] {
+        return false;
+      }
+    }
+    true
+  }
+
+  fn recurse_game(&self, hand: &Vec<i32>) -> usize {
+    let mut players: Vec<Vec<i32>> = Vec::new();
+    for p in 0..hand.len() {
+      players.push(self.players[p][..hand[p] as usize].iter().map(|&c| c).collect());
+    }
+    let mut sub_game = WarGame{players, previous: HashSet::new()};
+    loop {
+      if let Some(winner) = sub_game.next() {
+        return winner
+      }
+    }
+  }
+
+  fn next(&mut self) -> Option<usize> {
+    // If we're repeating the previous state, player 0 wins
+    if self.previous.contains(&self.players) {
+      for p in 1..self.players.len() {
+        self.players[p].clear();
+      }
+      return Some(0)
+    }
+    // Save the current state into previous
+    self.previous.insert(self.players.clone());
     // get the list of top cards from each hand
     let mut hand: Vec<i32> = self.players.iter_mut()
       .map(|h| h.remove(0)).collect();
     // figure out the winner
-    let winner = hand.iter().enumerate()
-      .reduce(|acc, c| if c.1 > acc.1 {c} else {acc}).unwrap().0;
-    // sort the hand
-    hand.sort_unstable_by(|l,r| i32::cmp(r,l));
+    let winner;
+    if self.can_recurse(&hand) {
+      winner = self.recurse_game(&hand);
+    } else {
+      winner = hand.iter().enumerate()
+        .reduce(|acc, c| if c.1 > acc.1 {c} else {acc}).unwrap().0;
+    }
+    let winning_card = hand.remove(winner);
+    hand.insert(0, winning_card);
     // put the cards back on the winner's deck
     self.players[winner].append(&mut hand);
     // remove a player that is out of cards
     self.players.retain(|h| !h.is_empty());
+    if self.is_done() {
+      Some(winner)
+    } else {
+      None
+    }
   }
 
   fn is_done(&self) -> bool {
