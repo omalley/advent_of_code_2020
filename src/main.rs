732fd3cabb61ebeafdccf594338f833a0fb34564
Owen O'Malley <omalley@apache.org>
2022-03-09 19:28:00 -0800

    puzzle 22.1

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,64 @@
-use std::collections::{HashMap, HashSet};
 use std::io;
 use std::io::BufRead;
 
-use lazy_static::lazy_static;
-use regex::Regex;
-
 #[derive(Debug)]
-struct Food {
-  ingredients: Vec<String>,
-  allergens: Vec<String>,
+struct WarGame {
+  players: Vec<Vec<i32>>,
 }
 
-impl Food {
-  fn parse(input: &str) -> Self {
-    lazy_static! {
-      static ref FOOD_PATTERN: Regex =
-         Regex::new(r"^(?P<ingredients>[^)]*)\s*\(contains\s+(?P<allergens>[^)]*)\)$").unwrap();
+impl WarGame {
+  fn parse(input: &mut dyn Iterator<Item=String>) -> Self {
+    let mut players: Vec<Vec<i32>> = Vec::new();
+    while let Some(line) = input.next() {
+      if !line.starts_with("Player") {
+        panic!("Bad input line {}", line);
+      }
+
+      let mut hand: Vec<i32> = Vec::new();
+      while let Some(line) = input.next() {
+        match line.parse::<i32>() {
+          Ok(card) => hand.push(card),
+          _ => break,
+        }
+      }
+      players.push(hand);
     }
-    let food_match = FOOD_PATTERN.captures(input).unwrap();
-    let ingredients = food_match.name("ingredients").unwrap().as_str().split_whitespace().map(|x| x.to_string()).collect();
-    let allergens = food_match.name("allergens").unwrap().as_str().split(", ").map(|x| x.to_string()).collect();
-    Food{ingredients, allergens}
+    WarGame{players}
+  }
+
+  fn next(&mut self) {
+    // get the list of top cards from each hand
+    let mut hand: Vec<i32> = self.players.iter_mut()
+      .map(|h| h.remove(0)).collect();
+    // figure out the winner
+    let winner = hand.iter().enumerate()
+      .reduce(|acc, c| if c.1 > acc.1 {c} else {acc}).unwrap().0;
+    // sort the hand
+    hand.sort_unstable_by(|l,r| i32::cmp(r,l));
+    // put the cards back on the winner's deck
+    self.players[winner].append(&mut hand);
+    // remove a player that is out of cards
+    self.players.retain(|h| !h.is_empty());
+  }
+
+  fn is_done(&self) -> bool {
+    self.players.len() == 1
+  }
+
+  fn score(&self) -> i32 {
+    let card_count = self.players[0].len();
+    self.players[0].iter().enumerate()
+      .fold(0, |acc, (posn, card) | acc + card * (card_count - posn) as i32)
   }
 }
 
 fn main() {
   let stdin = io::stdin();
-  let foods: Vec<Food> = stdin.lock().lines()
-    .map(|x| Food::parse(x.unwrap().trim())).collect();
-  let mut possible_ingredients: HashMap<String, HashSet<String>> = HashMap::new();
-  for f in &foods {
-    let ingredients: HashSet<String> = f.ingredients.iter()
-      .map(|s| s.to_string()).collect();
-    for allergen in &f.allergens {
-      if let Some(prev) = possible_ingredients.get_mut(allergen) {
-        prev.retain(|i| ingredients.contains(i));
-      } else {
-        possible_ingredients.insert(allergen.clone(), ingredients.clone());
-      }
-    }
+  let mut game = WarGame::parse(&mut stdin.lock().lines()
+    .map(|x| x.unwrap().trim().to_string()));
+  while !game.is_done() {
+    println!("{:?}", game);
+    game.next();
   }
-  println!("possible = {:?}", possible_ingredients);
-  // build a map of ingredient to allergen by picking any allergen that has a single
-  // ingredient and then removing it from the others
-  let mut contains: HashMap<String, String> = HashMap::new();
-  while !possible_ingredients.is_empty() {
-    let mut new_bindings : Vec<(String,String)> = Vec::new();
-    for (k, v) in &possible_ingredients {
-      if v.len() == 1 {
-        new_bindings.push((k.clone(), v.iter().next().unwrap().clone()))
-      }
-    }
-    for (a,i) in new_bindings {
-      contains.insert(i, a.clone());
-      possible_ingredients.remove(&a);
-    }
-    // remove the ingredient from the other allergens
-    for allergens in &mut possible_ingredients.values_mut() {
-      allergens.retain(|i| !contains.contains_key(i));
-    }
-  }
-  // Now we need to sort by allergen
-  let mut contains: Vec<(String,String)> = contains.iter()
-    .map(|(i, a)| (a.clone(), i.clone())).collect();
-  contains.sort_unstable();
-  // and print out the ingredients
-  let mut first = true;
-  for (_, i) in contains {
-    print!("{}{}", if first {""} else {","}, i);
-    first = false;
-  }
-  println!();
+  println!("score = {}", game.score());
 }
